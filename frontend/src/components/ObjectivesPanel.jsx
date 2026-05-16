59c046505532ed034fe28fdf30efa0876dabb25e
Krishna-7126 <krishnabhanderiya7126@gmail.com>
2026-05-16 15:57:12 +0530

    modified:   frontend/src/components/ObjectivesPanel.jsx

--- frontend/src/components/ObjectivesPanel.jsx
+++ frontend/src/components/ObjectivesPanel.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import LoadingSpinner from './LoadingSpinner';
 import { getObjectives } from '../api/v3Client';
 
