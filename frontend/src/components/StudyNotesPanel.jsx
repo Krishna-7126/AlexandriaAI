0ad6e4a46ddd10dc8380da3a798d29c4a222e68c
Krishna-7126 <krishnabhanderiya7126@gmail.com>
2026-05-16 15:57:46 +0530

    modified:   frontend/src/components/StudyNotesPanel.jsx

--- frontend/src/components/StudyNotesPanel.jsx
+++ frontend/src/components/StudyNotesPanel.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import LoadingSpinner from './LoadingSpinner';
 import { getStudyNotes } from '../api/v3Client';
 
