bc069daaee39a9e9e04be0a83350589c50b33629
Krishna-7126 <krishnabhanderiya7126@gmail.com>
2026-05-16 15:54:50 +0530

    modified:   frontend/src/main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
-import { AuthProvider } from './context/AuthContext'
+import { AuthProvider } from './context/AuthContext.jsx'
 import './index.css'
 import './responsive.css'
 import './styles/auth.css'
