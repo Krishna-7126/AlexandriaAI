f74a6f1fae45fd094a4cd0e68c44d4d67003a9b5
Krishna-7126 <krishnabhanderiya7126@gmail.com>
2026-05-17 17:38:15 +0530

    modified:   frontend/src/pages/SignupPage.jsx

--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
-import { useAuth } from '../context/AuthContext';
+import { useAuth } from '../context/authContext';
 import { Mail, Lock, User, Globe, AlertCircle, Loader, CheckCircle } from 'lucide-react';
 import customLogo from '../assets/logo.png';
 import '../styles/auth.css';
